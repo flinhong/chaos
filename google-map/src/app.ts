3b1eda0066d05f8ba791d40d9916954c2c3fdc90
Frank Lin <me@linhong.eu.org>
2022-05-30 00:57:26 +0800

    google maps as practise

---
+++ google-map/src/app.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const Google_API_Key = 'xxxx';
+
+// declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat:number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = encodeURI(addressInput.value)
+
+    // send this to Google's API
+    axios
+      .get<GoogleGeocodingResponse>(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${enteredAddress}&key=${Google_API_Key}`
+      )
+      .then((response) => {
+        // console.log(response);
+        if (response.data.status !== 'OK') {
+          throw new Error('Could not fetch location!');
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById('map')!, {
+          center: coordinates,
+          zoom: 12
+        })
+        new google.maps.Marker({position: coordinates, map: map});
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.log(err);
+      });
+}
+
+form.addEventListener('submit', searchAddressHandler)
---
+++ google-map/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: {
+        main: "./src/app.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [{
+            test: /.tsx?$/,
+            loader: "ts-loader"
+        }]
+    }
+}
